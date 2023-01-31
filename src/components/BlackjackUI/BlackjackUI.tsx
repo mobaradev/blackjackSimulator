5bd640c2828a9a6edf55f0515a94debb36ac516c
Michał Obara <mobaradev@yahoo.com>
2023-01-31 01:32:58 -0800

    Blackjack base gameplay implemented (Start, Hit, Stand)

--- src/components/BlackjackUI/BlackjackUI.tsx
+++ src/components/BlackjackUI/BlackjackUI.tsx
@@ -2,6 +2,8 @@ import styled from "styled-components";
 import AppController from "../../AppController";
 import {useEffect, useState} from "react";
 import CardUI from "../CardUI/CardUI";
+import Clearfix from "../Base/Clearfix/Clearfix";
+import Card from "../../logic/Card/Card";
 
 const Container = styled.div`
   width: 800px;
@@ -12,42 +14,96 @@ const Container = styled.div`
 `;
 
 function BlackjackUI(props: any) {
-    const [playerCards, setPlayerCards] = useState<number[]>([]);
-    const [dealerCards, setDealerCards] = useState<number[]>([]);
+    const [playerCards, setPlayerCards] = useState<Card[]>([]);
+    const [dealerCards, setDealerCards] = useState<Card[]>([]);
+    const [gameId, setGameId] = useState<number>(0);
 
     useEffect(() => {
-        AppController.onStart = start;
+        AppController.onGameStart = gameStart;
+        AppController.onGameEnd = gameEnd;
         AppController.onHit = hit;
+        AppController.onDealerHit = dealerHit;
     }, []);
 
-    const start = () => {
+    const gameStart = () => {
+        // clear
+        setGameId(gameId => gameId + 1);
+        setPlayerCards([]);
+        setDealerCards([]);
+
         console.log(AppController.blackjack.playerCards)
         for (let i = 0; i < AppController.blackjack.playerCards.length; i++) {
-            addPlayerCard(AppController.blackjack.playerCards[i].number);
+            addPlayerCard(AppController.blackjack.playerCards[i]);
         }
+
+        addDealerCard(AppController.blackjack.dealerCards[0]);
         console.log(AppController.blackjack.dealerCards)
 
     }
 
-    const hit = () => {
-        addPlayerCard(AppController.blackjack.playerCards[AppController.blackjack.playerCards.length - 1].number);
+    const gameEnd = () => {
+        setTimeout(() => {
+            if (AppController.blackjack.playerPoints > 21) {
+                alert("Player busted!");
+            } else {
+                if (AppController.blackjack.dealerPoints > 21) {
+                    alert("Dealer busted!");
+                } else {
+                    if (AppController.blackjack.dealerPoints > AppController.blackjack.playerPoints) {
+                        alert("Dealer won!")
+                    } else if (AppController.blackjack.dealerPoints < AppController.blackjack.playerPoints) {
+                        alert("Player won!")
+                    } else {
+                        alert("Draw!")
+                    }
+                }
+            }
+        }, 600);
     }
 
-    const addPlayerCard = (number: number) => {
-        setPlayerCards(playerCards => [...playerCards, number])
+    const hit = () => {
+        addPlayerCard(AppController.blackjack.playerCards[AppController.blackjack.playerCards.length - 1]);
+    }
+
+    const dealerHit = () => {
+        addDealerCard(AppController.blackjack.dealerCards[AppController.blackjack.dealerCards.length - 1])
+    }
+
+    const addPlayerCard = (card: Card) => {
+        setPlayerCards(playerCards => [...playerCards, card])
+    }
+
+    const addDealerCard = (card: Card) => {
+        setDealerCards(dealerCards => [...dealerCards, card])
     }
 
 
     return (
         <Container>
             Blackjack
+            <Clearfix />
 
-            Cards:
+            <h2>Player {AppController.blackjack.dealerPoints}</h2>
+            <Clearfix />
+            <>
             {
-                playerCards.map(
-                    (card, index) => <CardUI key={index} number={card} />
+                dealerCards.map(
+                    (card, index) => <CardUI key={gameId + "-" + index} number={card.value} />
                 )
             }
+            </>
+
+            <Clearfix />
+
+            <h2>Player {AppController.blackjack.playerPoints}</h2>
+            <Clearfix />
+            <>
+            {
+                playerCards.map(
+                    (card, index) => <CardUI key={gameId + "-" + index} number={card.value} />
+                )
+            }
+            </>
         </Container>
     )
 }
--- src/logic/Blackjack/Blackjack.ts
+++ src/logic/Blackjack/Blackjack.ts
@@ -6,30 +6,73 @@ class Blackjack {
     deck: Deck;
     playerCards: Card[];
     dealerCards: Card[];
+    isLocked: boolean;
 
     constructor() {
         this.deck = new Deck();
+        this.isLocked = false;
 
         this.playerCards = [];
         this.dealerCards = [];
-
-        console.log(this.deck.getCard());
-        console.log(this.deck.getCard());
-        console.log(this.deck.getCard());
     }
 
     start() {
+        this.clear();
         this.playerCards.push(this.deck.getCard())
         this.playerCards.push(this.deck.getCard())
         this.dealerCards.push(this.deck.getCard())
 
-        AppController.onStart();
+        AppController.onGameStart();
+    }
+
+    clear() {
+        this.deck = new Deck();
+        this.playerCards = [];
+        this.dealerCards = [];
     }
 
     hit() {
+        if (this.isLocked) return;
         this.playerCards.push(this.deck.getCard())
 
         AppController.onHit();
+
+        if (this.playerPoints > 21) {
+            AppController.onGameEnd();
+        }
+    }
+
+    dealerHit() {
+        if (this.isLocked) return;
+        this.dealerCards.push(this.deck.getCard())
+
+        AppController.onDealerHit();
+
+        if (this.dealerPoints > 21) {
+            AppController.onGameEnd();
+        }
+    }
+
+    stand() {
+        while (this.dealerPoints < 17) {
+            this.dealerHit();
+        }
+
+        AppController.onGameEnd();
+    }
+
+    get playerPoints() : number {
+        let playerPoints = 0;
+        this.playerCards.forEach(card => playerPoints += card.value);
+
+        return playerPoints;
+    }
+
+    get dealerPoints() : number {
+        let dealerPoints = 0;
+        this.dealerCards.forEach(card => dealerPoints += card.value);
+
+        return dealerPoints;
     }
 
 }
