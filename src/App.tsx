a324fd9df58ef921ca55afd703034e3be108c5e3
Michał Obara <mobaradev@yahoo.com>
2023-01-31 00:14:25 -0800

    Panel and Blackjack (UI) added to main App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import Panel from "./components/Panel/Panel";
+import BlackjackUI from "./components/BlackjackUI/BlackjackUI";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  overflow: auto;
+  background-color: aliceblue;
+`;
+
+const Content = styled.div`
+  width: 100%;
+  height: calc(100% - 120px);
+  float: left;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+        <Content>
+            <BlackjackUI />
+        </Content>
+        <Panel />
+    </Container>
   );
 }
 
