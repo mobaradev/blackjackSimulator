977245b86eaa298a9fc59482db7a2815992852a9
Michał Obara <mobaradev@yahoo.com>
2023-01-31 01:32:07 -0800

    AppController more game events added

--- src/AppController.ts
+++ src/AppController.ts
@@ -2,8 +2,13 @@ import Blackjack from "./logic/Blackjack/Blackjack";
 
 class AppController {
     static blackjack: Blackjack;
-    static onStart: () => void;
+    static onGameStart: () => void;
     static onHit: () => void;
+    static onStand: () => void;
+    static onDealerHit: () => void;
+    static onGameEnd: () => void;
+    // static onPlayerBust: () => void;
+    // static onDealerBust: () => void;
 
     static init() {
         AppController.blackjack = new Blackjack();
