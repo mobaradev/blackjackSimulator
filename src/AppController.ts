0b8547dc87086bf0929b5f85d5904c0dbe5c0ca1
Michał Obara <mobaradev@yahoo.com>
2023-02-05 01:49:45 -0800

    updateListeners added to onUpdate to handle more than one listener, statistics and predictions added to AppController

--- src/AppController.ts
+++ src/AppController.ts
@@ -1,18 +1,30 @@
 import Blackjack from "./logic/Blackjack/Blackjack";
+import Statistics from "./logic/Statistics/Statistics";
+import Predictions from "./logic/Predictions/Predictions";
 
 class AppController {
+    static statistics: Statistics;
+    static predictions: Predictions;
     static blackjack: Blackjack;
     static onGameStart: () => void;
     static onHit: () => void;
     static onStand: () => void;
     static onDealerHit: () => void;
     static onGameEnd: () => void;
-    static onUpdate: () => void;
+    static updateListeners: (() => void)[];
+    static onUpdate() {
+        this.updateListeners.forEach(updateListener => updateListener());
+    }
+
+
     // static onPanelUpdate(): () => void;
     // static onPlayerBust: () => void;
     // static onDealerBust: () => void;
 
     static init() {
+        AppController.updateListeners = [];
+        AppController.statistics = new Statistics();
+        AppController.predictions = new Predictions();
         AppController.blackjack = new Blackjack();
     }
 }
